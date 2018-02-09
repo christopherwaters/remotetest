14a7a8c20d816298bfdfbc07e51371018f98ae7d
Chris Waters <cdw2be@virginia.edu>
2018-02-09 14:12:55 -0500

    GUI basics formed

--- modelGUI.py
+++ modelGUI.py
@@ -5,11 +5,25 @@ Created on Fri Feb 9 12:48:42 2017
 @author: cdw2be
 """
 
-import Tkinter
+import tkinter as tk
 import mrimodel
 import confocalmodel
 import mesh
 
-class modelGUI():
-    """Generates a GUI to control the Python-based cardiac modeling toolbox.
-    """
+class modelGUI(tk.Frame):
+	"""Generates a GUI to control the Python-based cardiac modeling toolbox.
+	"""
+	
+	def __init__(self, master=None):
+		tk.Frame.__init__(self, master)
+		self.grid()
+		self.createWidgets()
+
+	def createWidgets(self):
+		self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+		self.quitButton.grid()
+
+root = tk.Tk()
+gui = modelGUI(master=root)
+gui.mainloop()
+root.destroy()
