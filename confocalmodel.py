f3150da8091d6a78e3b45b7912ad76ffd05dad5d
Chris Waters <cdw2be@virginia.edu>
2018-02-22 13:36:55 -0500

    Added new subclass to confocalmodel to hold slice information

--- confocalmodel.py
+++ confocalmodel.py
@@ -20,12 +20,13 @@ import glob
 from PIL import Image
 import os
 from cardiachelpers import importhelper
+from cardiachelpers import confocalhelper
 
 class ConfocalModel():
 	"""Model class to hold confocal microscopy images and format them to generate a mesh to align with MRI data.
 	"""
 
-	def __init__(self, confocal_dir):
+	def __init__(self, top_dir):
 		"""Initialize the model made to import confocal microscopy data.
 		
 		args:
@@ -82,28 +83,58 @@ class ConfocalModel():
 		image_channels_arr = np.array(image_channels)
 		return(image_channels_arr)
 	
-	def stitchImages(self, images=None, overlap=0.1):
-		"""Stitch images together in grid, based on grid size definitions and image numbering.
-		"""
-		if not images:
-			images = self.raw_images
-		# Get the image positions and pixel-micron conversion data
-		image_positions, column_dict = importhelper.getImagePositions(self.tif_files)
-		# Create a grid to match the number of positions possible, then fill images into the grid
-		x_slots = np.unique(image_positions[:, 0])
-		y_slots = np.unique(image_positions[:, 1])
-		image_grid = np.empty((x_slots.size, y_slots.size))
-		for image_num in range(image_positions.shape[0]):
-			x_slot = np.where(x_slots == image_positions[image_num, column_dict['XLocationMicrons']])[0]
-			y_slot = np.where(y_slots == image_positions[image_num, column_dict['YLocationMicrons']])[0]
-			image_grid[x_slot, y_slot] = image_num
-		# Get the size in pixel count for the images
-		image_x, image_y = images[0].size
-		image_z = images[0].n_frames
-		image_size = [image_x, image_y, image_z]
-		return(image_grid)
-	
 	def validateIntensity(self):
 		"""Adjust image intensity based on edge intensity of adacent images.
 		"""
 		pass
+		
+class ConfocalSlice():
+	"""Class to hold information for a single biological slice of tissue.
+	
+	Biological slices are stored in individual directories.
+	"""
+	def __init__(self, confocal_dir):
+		self.confocal_dir = confocal_dir
+		self.slice_name = os.path.split(confocal_dir)[1]
+		
+		self.tif_files = glob.glob(os.path.join(confocal_dir, '*.tif'))
+		self.raw_images = [None]*len(self.tif_files)
+		
+		for file_num, image_file in enumerate(self.tif_files):
+			self.raw_images[file_num] = Image.open(image_file)
+			
+		self.num_slices = self.raw_images[0].n_frames
+		
+		self.compressed_images = [None]*self.num_slices
+		
+		self.stitched_image_files = [None]*self.num_slices
+	
+	def createStitchedImage(self, overlap=0.1, compress_ratio=0.25, channel=-1, frame=0, stitched_file=False):
+		"""Stitch images together in a grid.
+		"""
+		self.im_locs, im_locs_dict = confocalhelper.getImagePositions(self.tif_files)
+		
+		self.im_grid = confocalhelper.getImageGrid(self.tif_files, self.im_locs, im_locs_dict)
+		
+		if not self.compressed_images[frame]:
+			self.compressed_images[frame] = [None]*len(self.raw_images)
+			
+		for raw_image, image_num in enumerate(self.raw_images):
+			image_frame = confocalhelper.splitImageFrames(raw_image)[frame]
+			img_channels = confocalhelper.splitChannels(image_frame)
+			if channel >= 0:
+				img_channels = list(img_channels[channel])
+			if compress_ratio < 1:
+				compressed_channels = [None]*len(img_channels)
+				for channel, channel_num in enumerate(img_channels):
+					compressed_channels[channel_num] = confocalhelper.compressImages(channel, image_scale=compress_ratio)
+				if len(compressed_channels) > 1:
+					compressed_image = Image.merge('RGB', compressed_channels)
+					self.compressed_images[frame][image_num] = compressed_image
+				else:
+					self.compressed_images[frame][image_num] = compressed_channels[0]
+		
+		if not stitched_file:
+			stitched_file = self.confocal_dir + 'stitched_slice=' + str(frame) + 'chan=' + str(channel) + '*.tif'
+			
+		stitched_success = confocalhelper.createStitchedImage(self.compressed_images[frame], self.im_grid[:, 0], self.im_grid[:, 1], save_pos=stitched_file)
--- functiontesting.py
+++ functiontesting.py
@@ -1,19 +1,9 @@
 import glob
 import os
+import confocalmodel
+
 confocal_dir = 'C:/Users/cdw2be/Documents/pythoncardiacmodel/Test Data/ConfocalTestLargest'
-tif_files = glob.glob(os.path.join(confocal_dir, '*.tif'))
-raw_images = [None]*len(tif_files)
-for file_num in range(len(tif_files)):
-	raw_images[file_num] = Image.open(tif_files[file_num])
-	
-im_locs, im_locs_dict = confocalhelper.getImagePositions(tif_files)
-im_grid = confocalhelper.getImageGrid(tif_files, im_locs, im_locs_dict)
 
-compressed_images = [None]*len(raw_images)
-for image_num in range(len(raw_images)):
-	image_frame_split = confocalhelper.splitImageFrames(raw_images[image_num])
-	image_red_chan = confocalhelper.splitChannels(image_frame_split[10], pull_channel=0)
-	compressed_images[image_num] = confocalhelper.compressImages(image_red_chan, image_scale=0.33)
+con_slice = confocalmodel.ConfocalSlice(confocal_dir)
 
-stitched_image_file = 'C:/Users/cdw2be/Documents/pythoncardiacmodel/Test Data/testimage.tif'
-confocalhelper.stitchImages(compressed_images, im_grid[:, 0], im_grid[:, 1], stitched_image_file)
+con_slice.createStitchedImage(channel=0)
